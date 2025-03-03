828d1e5ee25e2f8942663b6c746c2053a64ad1a0
qifuxiao <867225266@qq.com>
2025-03-03 16:50:58 +0800

    change logo

---
+++ docker/build_v1.sh
@@ -0,0 +1,4 @@
+docker login  registry.cn-chengdu.aliyuncs.com -u 867225266@qq.com -p Js147258
+docker build --network=host   -t registry.cn-chengdu.aliyuncs.com/llm-studio/chatchat:20250303.v2  -f Dockerfile . 
+
+docker push registry.cn-chengdu.aliyuncs.com/llm-studio/chatchat:20250303.v2
